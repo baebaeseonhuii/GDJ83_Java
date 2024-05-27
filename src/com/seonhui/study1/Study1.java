7c3225647f6d00d7e671baf22d8c9a8677697e92
baebaeseonhuii <qotjsgml3@naver.com>
2024-05-27 10:56:12 +0900

    first commit

---
+++ src/com/seonhui/study1/Study1.java
@@ -0,0 +1,10 @@
+package com.seonhui.study1;
+
+public class Study1 {
+	
+	//자바 실행 환경 (jre, jvm)
+	public static void main(String [] args) {
+		// 프로그램 실행 시 최초 실행
+		//프로그램 종료는 메인 메서드가 종료되면 끝남
+	}
+}
