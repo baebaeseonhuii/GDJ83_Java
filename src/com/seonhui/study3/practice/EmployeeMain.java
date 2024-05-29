99dcf1a4beb9b9451c36d9ad7cbb0818fc318ab5
baebaeseonhuii <qotjsgml3@naver.com>
2024-05-29 18:15:47 +0900

    practiceVer

---
+++ src/com/seonhui/study3/practice/EmployeeMain.java
@@ -0,0 +1,18 @@
+package com.seonhui.study3.practice;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+//		e1.bonus = 1000;
+//		e1.departmentName = "IT Team";
+//		e1.employeeNum = 970925;
+//		e1.name = "민병관";
+//		e1.salary = 50000000;
+//
+//		e1.mockInfo();
+		e1.sayInfo();
+
+	}
+
+}
