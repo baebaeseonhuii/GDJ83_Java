188ed013c2d4414dde42452a6b1ada7c2d2a286e
baebaeseonhuii <qotjsgml3@naver.com>
2024-05-31 10:26:38 +0900

    cannot change values when it comes to final

--- src/com/seonhui/s4/S4Main.java
+++ src/com/seonhui/s4/S4Main.java
@@ -6,7 +6,7 @@ public class S4Main {
 		// JVM이 시작될 때 "main method"를 찾아서 실행하기 때문에 main이름은 바꾸면 안됨
 		// 클래스 변수
 		// 클래스명.변수명
-		Car.company = "";
+
 		System.out.println(Car.company);
 
 		Car.info();
